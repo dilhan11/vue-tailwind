e4813d394817f80d4dceaca0896b141d199a729c
Dilhan Hettiarachchige <dilhannitro@gmail.com>
2022-04-02 20:19:20 +0530

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      DMsans: ['DMsans,san-serif']
+    }
   },
-  plugins: [],
+  plugins: []
 }
